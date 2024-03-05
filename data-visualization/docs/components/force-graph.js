ec6b7615e574f04b7bce8b48694f18c4b4d240d0
Diego Vinasco-Alvarez <diego.vinasco@protonmail.com>
2024-03-05 19:31:46 +0100

    add initial tests

---
+++ data-visualization/docs/components/force-graph.js
@@ -0,0 +1,27 @@
+import { map, filter } from "npm:d3-array";
+
+export function mapProductsToGraph(data) {
+  // create triples for each project and add them to an array (representing the graph)
+  const projectGraph = [];
+
+  data.forEach((project) => {
+    // link to root node
+    projectGraph.push(["PEPR VDBI", "hasProjet", project.id]);
+    // each project has a key/value pair
+    for (const [key, value] of Object.entries(project)) {
+      // skip id cells
+      if (key != "id") {
+        // each value is an array of properties
+        value.forEach((element) => {
+          projectGraph.push([
+            project.id, // project id
+            key, // property name
+            element, // unique value
+          ]);
+        });
+      }
+    }
+  });
+
+  return projectGraph;
+}
---
+++ data-visualization/docs/components/import-products.js
@@ -0,0 +1,80 @@
+import { map, filter } from "npm:d3-array";
+
+export function getProductSheet(workbook) {
+  return workbook.sheet(workbook.sheetNames[1], {
+    range: "A1:AG78",
+    headers: true,
+  });
+}
+
+export function resolveKnownEntities(sheet) {
+  // Map raw project data to a simple 2D array
+  const projectMap = map(sheet, (d, i) => {
+    return {
+      id: [i],
+      acronyme: [d["Acronyme"]],
+      nom: [d["NOM et prénom"]],
+      titre: [d["Titre complet"]],
+      action: [d["ACTION (de recherche)"]],
+      comment: [d["COMMENT"]],
+      pourquoi: [d["POUR QUOI FAIRE"]],
+      proposition: [d["proposition de recherche (format synthétique)"]],
+      actionPourSolutions: [d["Quels actions POUR quelles solutions ?"]],
+      produit: filter(
+        [
+          d["Produit (ou résultat) de la recherche (primaire)"],
+          d["Produit (ou résultat) de la recherche (Secondaire)"],
+        ],
+        (d) => {
+          return typeof d !== "undefined";
+        }
+      ),
+      objetOuDispositifImplique: filter(
+        [
+          d["objets et dispositifs urbains impliqués 1"],
+          d["objets et dispositifs urbains impliqués 2"],
+          d["Objets et dispositifs urbains impliqués  3"],
+        ],
+        (d) => {
+          return typeof d !== "undefined";
+        }
+      ),
+      motClef: filter(
+        [
+          d["Mot clef 1"],
+          d["Mot clef 2"],
+          d["Mot clef 3"],
+          d["Mot clef 4"],
+          d["Mot clef 5"],
+          d["Mot clef 6"],
+          d["Mot clef 7"],
+          d["Mot clef 8"],
+          d["Mot clef 9"],
+          d["Mot clef 10"],
+          d["Mot clef 11"],
+          d["Mot clef 12"],
+          d["Mot clef 13"],
+        ],
+        (d) => {
+          return typeof d !== "undefined";
+        }
+      ),
+      defi: filter(
+        [
+          d["Defi principal"],
+          d["autre défi 1"],
+          d["autre défi 2"],
+          d["autre défi 3"],
+          d["autre défi 4"],
+          d["autre défi 5"],
+          d["autre défi 6"],
+        ],
+        (d) => {
+          return typeof d !== "undefined";
+        }
+      ),
+    };
+  });
+
+  return projectMap;
+}
