5dc03ac4eb0b5775d8cd965936d87a9d053bf8e7
Diego Vinasco-Alvarez <diego.vinasco@proton.me>
2025-03-05 15:55:53 +0100

    update r2r tests

--- data-integration/up_test_r2r.sh
+++ data-integration/up_test_r2r.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 
-ollama serve
+ollama serve &
 docker start postgres-r2r-test
-export R2R_CONFIG_PATH=$PWD/test-data/r2r_config.toml
+
+export R2R_CONFIG_PATH=$PWD/test-data/r2r-test/r2r_config.toml
 source ./venv/bin/activate
 python -m r2r.serve
