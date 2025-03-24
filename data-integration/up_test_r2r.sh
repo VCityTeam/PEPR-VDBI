71f2fd0082fdb294037560b8a74ff75b474ec14d
Diego Vinasco-Alvarez <diego.vinasco@proton.me>
2025-03-24 18:00:18 +0100

    minor fixes

--- data-integration/up_test_r2r.sh
+++ data-integration/up_test_r2r.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
-ollama serve > ollama.log
+ollama serve & > ollama.log
 docker start postgres-r2r-test
 
 source ./venv/bin/activate
 export R2R_CONFIG_PATH=$PWD/test-data/r2r-test/r2r_config.toml
-python -m r2r.serve > r2r.log
+python -m r2r.serve & > r2r.log
