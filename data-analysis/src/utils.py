8f1e629e8afa8a7f02ebc4118fbd81df85d41aae
Diego Vinasco-Alvarez <diego.vinasco-alvarez@cnrs.fr>
2025-07-31 17:03:01 +0200

    add stemming and lemmatization from nltk to wordcloud workflow - refacto workflow I/O - refacto workflow configuration - add test workflow example

--- data-analysis/src/utils.py
+++ data-analysis/src/utils.py
@@ -1,46 +1,27 @@
-import csv
 import os
+import csv
 
 
-def readFile(file_path: str, encoding="UTF-8") -> str:
+def read_file(file_path: str, encoding="UTF-8") -> str:
     text = ""
     with open(file_path, "r", encoding=encoding) as file:
         text = file.read()
     return text
 
 
-def writeToFile(output_path: str, text: str, encoding="UTF-8"):
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
+def write_file(output_path: str, text: str, encoding="UTF-8"):
+    output_dir = os.path.dirname(output_path)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
     with open(output_path, "w", encoding=encoding) as file:
         file.write(text)
 
 
-def writeWordCounts(
-    word_counts: dict, output_file: str = "./wordcount.csv", limit: int | None = None
-):
-    """
-    Write a word_count to a csv file.
-    :word_counts: a dictionary where each key corresponds to a word and each value the
-        word count
-    :output_file: output file path
-    :limit: limit number of output rows
-    :sort: sort output by count?
-    """
-    output = list(word_counts.items())
-    output.sort(key=lambda x: x[1], reverse=True)
-
-    print(f"writing to csv {output_file}")
-    output_dir = os.path.split(output_file)[0]
+def write_csv(output_path: str, data: list, encoding="UTF-8"):
+    output_dir = os.path.dirname(output_path)
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-    with open(output_file, "w") as file:
-        writer = csv.writer(file)
-        writer.writerow(["weight", "word", "color", "url"])
-        row_count = 0
-
-        for row in output:
-            if limit and row_count > int(limit):
-                break
-            writer.writerow([row[1], row[0], "", ""])
-            row_count += 1
+    with open(output_path, "w", encoding=encoding) as file:
+        csv_writer = csv.writer(file)
+        for row in data:
+            csv_writer.writerow(row)
